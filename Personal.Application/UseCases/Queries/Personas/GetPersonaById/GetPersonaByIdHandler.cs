e29f7415e50f72cf0116881601684a52d7d3afea
RcerrutoBsol <102257758+RcerrutoBsol@users.noreply.github.com>
2022-07-30 10:26:27 -0400

    Update GetPersonaByIdHandler.cs

--- Personal.Application/UseCases/Queries/Personas/GetPersonaById/GetPersonaByIdHandler.cs
+++ Personal.Application/UseCases/Queries/Personas/GetPersonaById/GetPersonaByIdHandler.cs
@@ -30,7 +30,7 @@ namespace Personal.Application.UseCases.Queries.Personas.GetPersonaById {
 		  //Nombre = objPersona.NombreCompleto,
 		  //Tipo = objPersona.Tipo,
 		  //Tripulacion = objPersona.Tripulacion,
-		//};
+		};
 
 		//foreach (var item in objPersona.Regis)
 		//{
